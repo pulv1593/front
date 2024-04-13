dba6c07ed65a5e7d7ff5b4a89c0161a8901b8f77
pulv1593 <jiro0727@naver.com>
2024-04-13 21:58:27 +0900

    Feat : react router dom 설치 및 컴포넌트,페이지 폴더 생성
    페이지 라우팅을 위한 react router dom 설치 및 페이지 라우팅 설정하였음. 추가로 components, pages 구분을 위한 폴더를 추가하였음.

--- roulette-front/src/pages/Code/RequestCode.jsx
+++ roulette-front/src/pages/Code/RequestCode.jsx
@@ -1,14 +1,8 @@
 
 const CodeQuestion = () => {
   return (
-    <div className="MainRequest-container">
+    <div>
       질문글 작성하기 페이지
-      <div className="Request-container">
-        <input type="text" placeholder="제목"/>
-        <input type="text" placeholder="사진에 대한 설명을 상세히 적어주세요."/>
-        <input type="text" placeholder="사진"/>
-      </div>
-      <button className="RequestPost-btn"> 작성하기 </button>
     </div>
   )
 }
