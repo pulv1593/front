af3b3e4d31f27a9a7a56647ec1ab005440bf57b2
pulv1593 <jiro0727@naver.com>
2024-04-18 16:14:58 +0900

    feat: 마이페이지 질문, 답변 목록 페이지 ui 구현

--- roulette-front/src/pages/MyPage/RequestList.jsx
+++ roulette-front/src/pages/MyPage/RequestList.jsx
@@ -1,10 +1,22 @@
+import { Link } from "react-router-dom"
+import PostLayout from "../../components/PostLayout/PostLayout"
 import posts from "../../components/postData/postData"
 
 const RequestList = () => {
   
   return (
     <div>
-      내 질문 목록
+      <h1>내 질문 목록</h1>
+      <div className="questionBtn-container" style={{display: "flex", flexDirection:"row-reverse"}}>
+          <Link to="/code/ask" style={{
+            backgroundColor:"#ff9e9e",
+            margin:"5px",
+            padding:"5px 10px", 
+            border:"1px solid black",
+            color: "black", 
+            textDecoration: "none"}}> 질문하기 </Link>
+        </div>
+        <PostLayout />
     </div>
   )
 }
