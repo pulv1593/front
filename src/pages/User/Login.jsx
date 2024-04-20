e064f84b0cbb3ee7ec610095a7435fbfd8409e54
wjdwlgh2012 <wjdwlgh2012@naver.com>
2024-04-20 17:34:08 +0900

    feat: Fix kakao login 기능

--- src/pages/User/Login.jsx
+++ src/pages/User/Login.jsx
@@ -1,42 +1,38 @@
 	const Login = () => {
-  const REST_API_KEY = ;
-  const REDIRECT_URI = process.env.REACT_APP_REDIRECT_URI;
-  const link = REDIRECT_URI+`/kakaologin`;
+  const CLIENT_ID = process.env.REACT_APP_KAKAO_CLIENT_ID;
+  const REDIRECT_URI = process.env.REACT_APP_KAKAO_REDIRECT_URI;
+  const link = `https://kauth.kakao.com/oauth/authorize?scope=account_email&client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code&prompt=login`;;
 
   const loginHandler = () => {
     window.location.href = link;
   };
-
+	
   return (
-    <div style={{
-      
-    }}>
-      <div style={{
-        border: "1px solid #000000",
-        width: "40%",
-        height: "70%",
-        position: "absolute",
-        left: "50%",
-        top: "55%",
-        transform: "translate(-50%, -50%)"
-      }}>
-        <button type='button' onClick={loginHandler} style={{
-          width: "50%",
-          height: "60px",
-          fontSize: "20px",
-          borderRadius: "40px",
-          border: "0px",
-          backgroundColor: "yellow",
-          padding: "10px",
-          position: "absolute",
-          left: "50%",
-          top: "50%",
-          transform: "translate(-50%, -50%)"
-        }}>
-          카카오로 로그인 하기
-        </button>
-      </div>
-    </div>
+		<div style={{
+			border: "1px solid #000000",
+			width: "40%",
+			height: "70%",
+			position: "absolute",
+			left: "50%",
+			top: "55%",
+			transform: "translate(-50%, -50%)"
+		}}>
+			<button type='button' onClick={loginHandler} style={{
+				width: "50%",
+				height: "60px",
+				fontSize: "20px",
+				borderRadius: "40px",
+				border: "0px",
+				backgroundColor: "yellow",
+				padding: "10px",
+				position: "absolute",
+				left: "50%",
+				top: "50%",
+				transform: "translate(-50%, -50%)"
+			}}>
+				카카오로 로그인 하기
+			</button>
+		</div>
   );
 };
 
