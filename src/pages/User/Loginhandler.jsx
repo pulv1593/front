e093b49d002e4ec2a502b4d71e5513cad3959b83
kbsksy1221 <kbsksy1221@naver.com>
2024-04-22 00:02:30 +0900

    fix: app.jsx, loginhandler.jsx

--- src/pages/User/Loginhandler.jsx
+++ src/pages/User/Loginhandler.jsx
@@ -2,40 +2,29 @@ import { useEffect } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 
-const LoginHandeler = (props) => {
+const LoginHandeler = () => {
 	const navigate = useNavigate();
 	const code = new URL(window.location.href).searchParams.get("code");
 // 인가코드 백으로 보내는 작업 하는곳
 	useEffect(() => {
     const kakaoLogin = async () => {
-		try {
-			const res = await axios({
-					method: "GET",
-					url: `${import.meta.env.VITE_REDIRECT_URL}/?code=${code}`,
-					headers: {
-						"Content-Type": "application/json;charset=utf-8",
-					},
-			});
-			//백에서 완료후 우리사이트 전용 토큰 넘겨주는게 성공했다면
-        console.log(res);
-        	//localStorage에 저장할 정보
-				// localStorage.setItem("name", res.data.account.profile_nickname);
-				// localStorage.setItem("email", res.data.account.account_email);
-				// localStorage.setItem("profile_img", res.data.account.profile_img);
-				// localStorage.setItem("uaer_id", res.data.user_id);
-				// localStorage.setItem("access_token", res.data.access_token);
-			//로그인 성공시 코드게시판으로 link
+			try {
+				const res = await axios.post(`${import.meta.env.VITE_REDIRECT_URI}/auth/kakao/token`,{
+					code	
+				}),
+				localStorage.setItem("access_token", res.data.access_token);
+				console.log(res);
 				navigate("/code");
 			} catch (error){
 				console.error("Error occured", error);
-				console.log("ㅇㅇㅇ"+code);
-				// 로그인 실패 시 홈으로
-				// navigate("/");
-			}
-		};
-		kakaoLogin();
-	}, [props.history]);
-	
+				navigate("/");
+				}
+			};
+		if(code) {
+			kakaoLogin();
+		}
+	}, [code, navigate]);
+
   return (
     <div className="LoginHandeler">
       <div className="notice">
