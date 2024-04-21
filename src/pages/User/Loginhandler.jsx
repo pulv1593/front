e44efc6b5efbb12e9e1ad9e02ed04be19bbc34b8
kbsksy1221 <kbsksy1221@naver.com>
2024-04-21 22:33:46 +0900

    fix: loginhandler 수정

--- src/pages/User/Loginhandler.jsx
+++ src/pages/User/Loginhandler.jsx
@@ -12,10 +12,10 @@ const LoginHandeler = (props) => {
 			const res = await axios({
 					method: "GET",
 					url: `${import.meta.env.VITE_REDIRECT_URL}/?code=${code}`,
-					console.log(code);
+					console.log(code),
 					headers: {
-				 "Content-Type": "application/json;charset=utf-8",
-				},
+						"Content-Type": "application/json;charset=utf-8",
+					},
 			});
 			//백에서 완료후 우리사이트 전용 토큰 넘겨주는게 성공했다면
         console.log(res);
