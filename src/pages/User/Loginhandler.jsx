a9956e2cf728bfffe5fb00e1fa8094560c48c517
kbsksy1221 <kbsksy1221@naver.com>
2024-04-22 19:10:29 +0900

    fix: login fix

--- src/pages/User/Loginhandler.jsx
+++ src/pages/User/Loginhandler.jsx
@@ -6,7 +6,7 @@ const LoginHandeler = () => {
 	const navigate = useNavigate();
 	const back = 'https//k9bceeba41403a.user-app.krampoline.com';
 	// const back = import.meta.env.VITE_BACK_REDIRECT_URI;
-	// const back2 = 'k9bceeba41403a.user-app.krampoline.com/login/oauth2/code/kakao/'
+	const back2 = 'https//k9bceeba41403a.user-app.krampoline.com/login/oauth2/code/kakao/auth/kakao/token'
 	// const redirect_uri = 'https://k56733b335962a.user-app.krampoline.com/login/oauth2/callback/kakao';
 	const redirect_uri = import.meta.env.VITE_FRONT_KAKAO_REDIRECT_URI;
 	const code = new URL(window.location.href).searchParams.get("code");
@@ -16,7 +16,7 @@ const LoginHandeler = () => {
     const kakaoLogin = async () => {
 			try {
 				// 	https://k9bceeba41403a.user-app.krampoline.com/reqlogin/{인가코드}
-				const res = await axios.post(`${back}/reqlogin/${code}`);
+				const res = await axios.get(`${back}/reqlogin/${encodeURIComponent(code)}`);
 				localStorage.setItem("access_token", res.data.access_token);
 				console.log(res);
 				console.log("성공" + code);
