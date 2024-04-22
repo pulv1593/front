7e1e348902c2187602837dd34dbfa407eb3cd66a
kbsksy1221 <kbsksy1221@naver.com>
2024-04-22 18:47:27 +0900

    fix: login fix

--- src/pages/User/Loginhandler.jsx
+++ src/pages/User/Loginhandler.jsx
@@ -4,16 +4,18 @@ import { useNavigate } from "react-router-dom";
 
 const LoginHandeler = () => {
 	const navigate = useNavigate();
-	const back = 'https//k9bceeba41403a.user-app.krampoline.com/login/oauth2/code/kakao/auth/kakao/token';
-	const redirect_uri = 'https://k56733b335962a.user-app.krampoline.com/login/oauth2/callback/kakao';
+	// const back = 'https//k9bceeba41403a.user-app.krampoline.com';
+	const back = import.meta.env.VITE_BACK_REDIRECT_URI;
+	// const redirect_uri = 'https://k56733b335962a.user-app.krampoline.com/login/oauth2/callback/kakao';
+	const redirect_uri = import.meta.env.VITE_FRONT_KAKAO_REDIRECT_URI;
 	const code = new URL(window.location.href).searchParams.get("code");
+	
 // 인가코드 백으로 보내는 작업 하는곳
 	useEffect(() => {
     const kakaoLogin = async () => {
 			try {
-				const res = await axios.post(`${back}`,{
-					code
-				});
+				// 	https://k9bceeba41403a.user-app.krampoline.com/reqlogin/{인가코드}
+				const res = await axios.get(`${back}/reqlogin/${code}`);
 				localStorage.setItem("access_token", res.data.access_token);
 				console.log(res);
 				console.log("성공" + code);
@@ -28,7 +30,7 @@ const LoginHandeler = () => {
 			kakaoLogin();
 		}
 	}, [code, navigate]);
-
+	
   return (
     <div className="LoginHandeler">
       <div className="notice">
