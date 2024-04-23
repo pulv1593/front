1a0c80d11257249c656bcbfacf74381477865c3c
kbsksy1221 <kbsksy1221@naver.com>
2024-04-23 12:24:55 +0900

    feat: header, mypage 사용자 따라 정보 보여주기

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -49,63 +49,66 @@ const Header = () => {
 			  width: "100%",
 			}}/>
 		 </button>
-		 <div className="button" 
-			style={{
-			  height: "100%",
-			  padding: "20px",
-			  display: "flex",
-			  justifyContent: "space-between",
-			  alignItems: "center",
-			}}>
-			<button onClick={linkMail} 
-			  style={{
-				width: "50px",
-				height: "50px",
-				margin: "5px",
-				border: "0",
-				borderRadius: "50%",
-				backgroundColor: "#ffffff",
-			  }}>
-			  <img src={Mail} style={{
-				width: "30px",
-			  }}/>
-			  {/* 알람 개수 받아오기 */}
-			  <span className="noticeCount" style={{
-				position: "absolute",
-				top: "5px",
-				right: "75px",
-				padding: "5px 10px",
-				borderRadius: "50%",
-				background: "red",
-				color: "white",
-			  }}>2</span>
-			</button>
-			<div className="dropdown" 
-			  ref={dropDownRef}
-			  style={{
-				position: "relative",
-				display: "inline-block",
-			  }}>
-			  <button 
-				className="Profile" 
-				onClick={dropdownClicked}
+		  {token && (
+		  	<div className="button" 
 				style={{
-				  width: "50px",
-				  height: "50px",
-				  margin: "5px",
-				  border: "0",
-				  borderRadius: "50%",
-				  backgroundColor: "#ffffff",
-				}} >
-				<img src={User} style={{
-				  width: "30px",
-				}}/>
-			  </button>
-			  {isOpen && (
-				<Dropdown closeDrop={[isOpen, setIsOpen]}/>
-			  )}
+				  height: "100%",
+				  padding: "20px",
+				  display: "flex",
+				  justifyContent: "space-between",
+				  alignItems: "center",
+				}}>
+				<button onClick={linkMail} 
+				  style={{
+					width: "50px",
+					height: "50px",
+					margin: "5px",
+					border: "0",
+					borderRadius: "50%",
+					backgroundColor: "#ffffff",
+				  }}>
+				  <img src={Mail} style={{
+					width: "30px",
+				  }}/>
+				  {/* 알람 개수 받아오기 */}
+				  <span className="noticeCount" style={{
+					position: "absolute",
+					top: "5px",
+					right: "75px",
+					padding: "5px 10px",
+					borderRadius: "50%",
+					background: "red",
+					color: "white",
+				  }}>2</span>
+				</button>
+				<div className="dropdown" 
+				  ref={dropDownRef}
+				  style={{
+					position: "relative",
+					display: "inline-block",
+				  }}>
+				  <button 
+					className="Profile" 
+					onClick={dropdownClicked}
+					style={{
+					  width: "50px",
+					  height: "50px",
+					  margin: "5px",
+					  border: "0",
+					  borderRadius: "50%",
+					  backgroundColor: "#ffffff",
+					}} >
+					<img src={User} style={{
+					  width: "30px",
+					}}/>
+				  </button>
+				  {isOpen && (
+					<Dropdown closeDrop={[isOpen, setIsOpen]}/>
+				  )}
+				</div>
 			</div>
-		</div>
+		  )}
+		 
     </div>
   )
 }
--- src/pages/MyPage/Myinfo.jsx
+++ src/pages/MyPage/Myinfo.jsx
@@ -1,91 +1,122 @@
-import { useNavigate } from "react-router-dom";
-import kakao from '../../assets/KakaoTalk_logo.png'
-import './Myinfo.css'
+import { useNavigate, useState, useEffect } from "react-router-dom";
+import kakao from '../../assets/KakaoTalk_logo.png';
+import './Myinfo.css';
+import axios from 'axios';
 
 const Myinfo = () => {
-  const navigate = useNavigate();
+	const navigate = useNavigate();
 
-  const linkHome = () => {
-    navigate('/');
-  };
+	const linkHome = () => {
+	navigate('/');
+	};
+	
+	const [userInfo, setUserInfo] = setState({
+		name: '',
+		email: '',
+	});
+	
+	const token = localStorage.getItem('access_token');
+	
+	useEffect(() => {
+		const userInfo = async () => {
+			try {
+				const res = await.axios.get('백엔드 api url', {
+					handers: {
+						Authorization: `Bearer ${token}`
+					}
+				});
+				setUserInfo(res.data);
+			} catch (error) {
+				console.error('사용자 정보를 불러오는 중 오류가 발생했습니다.', error);
+				navigate('/');
+			}
+		};
+		if(token){
+			userInfo();
+		}
+	}, []);
 
-  return (
-    <div style={{
-      width: "auto",
-      height: "100%",
-      padding: "5% 10%",
-      fontWeight: "bold"
-    }}>
-      <h2 style={{
-        fontSize: "50px",
-      }}>My Page</h2>
-      <div style={{
-        padding: "30px",
-        marginBottom: "30px",
-        display: "flex",
-      }}>
-        <div style={{
-          padding: "10px",
-        }}>
-          <p style={{
-            fontSize: "20px", 
-          }}>프로필</p>
-          <img src="https://dummyimage.com/250x250/000/e0e0e0.png&text=profile+img"/>
-        </div>
-        <div style={{
-          width: "100%",
-          padding: "50px",
-          fontSize: "1.2rem",
-        }}>
-          {/* 받아온 이름 뿌려주기 */}
-          <p style={{
-            paddingTop: "10px",
-          }}>님</p>
-          <p style={{
-            paddingTop: "10px",
-          }}>자기 소개</p>
-          <input type="textarea" placeholder="자기소개" style={{
-            width: "90%",
-            height: "50%",
-          }}/>
-          {/* 본인이면 수정/탈퇴 버튼 보여주고, 본인이 아니면 안보여줌 */}
-          <button className="edit_btn">저장</button>
-        </div>
-      </div>
-      <div style={{
-        padding: "30px",
-        marginBottom: "30px",
-      }}>
-        <p style={{
-          fontSize: "25px",
-        }}>소셜 로그인 계정</p>
-        <div style={{
-          padding: "10%",
-          display: "flex",
-          alignItems: "center",
-        }}>
-          <img src={kakao} style={{
-            width: "80px",
-            height: "auto",
-          }}/>
-          <p style={{
-            paddingLeft: "25%",
-          }}>
-          {/* 소셜 이메일 */}
-          0000000@kakao.com
-          </p>
-        </div>
-      </div>
-      <div style={{
-        padding: "5%",
-        marginBottom: "30px",
-      }}>
-        <button onClick={linkHome} className="Cancel">
-          회원 탈퇴
-        </button>
-      </div>
-    </div>
-  );
+	return (
+	<div style={{
+	  width: "auto",
+	  height: "100%",
+	  padding: "5% 10%",
+	  fontWeight: "bold"
+	}}>
+	  <h2 style={{
+		fontSize: "50px",
+	  }}>My Page</h2>
+	  <div style={{
+		padding: "30px",
+		marginBottom: "30px",
+		display: "flex",
+	  }}>
+		<div style={{
+		  padding: "10px",
+		}}>
+		  <p style={{
+			fontSize: "20px", 
+		  }}>프로필</p>
+		  <img src="https://dummyimage.com/250x250/000/e0e0e0.png&text=profile+img"/>
+		</div>
+		<div style={{
+		  width: "100%",
+		  padding: "50px",
+		  fontSize: "1.2rem",
+		}}>
+		  {/* 받아온 이름 뿌려주기 */}
+		  <p style={{
+			paddingTop: "10px",
+		  }}>
+			  <span>{userInfo.name}</span>
+			님</p>
+		  <p style={{
+			paddingTop: "10px",
+		  }}>자기 소개</p>
+		  <input type="textarea" placeholder="자기소개" style={{
+			width: "90%",
+			height: "50%",
+		  }}/>
+		  {/* 본인이면 수정/탈퇴 버튼 보여주고, 본인이 아니면 안보여줌 */}
+			{token && (
+				<button className="edit_btn">저장</button>
+			)}
+		</div>
+	  </div>
+	  <div style={{
+		padding: "30px",
+		marginBottom: "30px",
+	  }}>
+		<p style={{
+		  fontSize: "25px",
+		}}>소셜 로그인 계정</p>
+		<div style={{
+		  padding: "10%",
+		  display: "flex",
+		  alignItems: "center",
+		}}>
+		  <img src={kakao} style={{
+			width: "80px",
+			height: "auto",
+		  }}/>
+		  <p value={} style={{
+			paddingLeft: "25%",
+		  }}>
+		  {/* 소셜 이메일 */}
+		  <span>{userInfo.email}</span>
+		  </p>
+		</div>
+	  </div>
+	  <div style={{
+		padding: "5%",
+		marginBottom: "30px",
+	  }}>
+		<button onClick={linkHome} className="Cancel">
+		  회원 탈퇴
+		</button>
+	  </div>
+	</div>
+	);
 };
 
 export default Myinfo;
