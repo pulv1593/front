64e58b666ab8808098aaad56bc7fb9d7359f2920
kbsksy1221 <kbsksy1221@naver.com>
2024-04-23 22:03:21 +0900

    fix: boardmain.jsx 수정

--- src/pages/Code/BoardMain.jsx
+++ src/pages/Code/BoardMain.jsx
@@ -5,48 +5,55 @@ import { useEffect, useState } from "react";
 
 const BoardMain = () => {
   const [posts, setPosts] = useState([]);
-  const [currentPage, setCurrentPage] = useState(0); // 페이지 default가 0
+  const [currentPage, setCurrentPage] = useState(0);
   const [totalPages, setTotalPages] = useState(1);
   
   const redirect_uri = import.meta.env.VITE_BACK_REDIRECT_URI;
-  const access_token = localStorage.getItem('access_token');
 
-// 	메인페이지 default : page = 0
+  const fetchPosts = async () => {
+		const access_token = localStorage.getItem('access_token');
+    try {
+      const response = await axios.get(`${redirect_uri}/post/list/${currentPage}`, {
+				headers: {
+        	Authorization: `Bearer ${access_token}`
+      	}
+			});
+      const res = response.data;
+      setPosts(res.posts);
+      setTotalPages(res.totalPages);
+    } catch (error) {
+      console.error('Error fetching posts:', error);
+    }
+  };
+
+  // 메인페이지 default : page = 0
   useEffect(() => {
-    const fetchPosts = async () => {
-      try {
-        const response = await axios.get(`${redirect_uri}/post/list/${currentPage}`);
-        const res = response.data;
-        setPosts(res.posts);
-        setTotalPages(res.totalPages); // 받아온 totalPages 값을 설정합니다.
-      } catch (error) {
-        console.error('Error fetching posts:', error);
-      }
-    };
+    fetchPosts();
+  }, [currentPage]); // currentPage가 변경될 때마다 fetchPosts 함수를 호출합니다.
 
   const handlePageChange = (pageNumber) => {
     setCurrentPage(pageNumber);
   };
 
   return (
-		<div>
+    <div>
       <h1>게시판</h1>
       <div className="BoardMain-container" style={{width: '100%', borderTop: '1px solid #ffe6e6'}} >
-				<div className="questionBtn-container" style={{ display: "flex", flexDirection: "row-reverse" }}>
-					<Link to="/post/ask" style={{
-        		backgroundColor: "#ff9e9e",
-        		margin: "5px",
-      			padding: "5px 10px",
-        		border: "1px solid black",
-        		color: "black",
-        		textDecoration: "none"
+        <div className="questionBtn-container" style={{ display: "flex", flexDirection: "row-reverse" }}>
+          <Link to="/post/ask" style={{
+            backgroundColor: "#ff9e9e",
+            margin: "5px",
+            padding: "5px 10px",
+            border: "1px solid black",
+            color: "black",
+            textDecoration: "none"
           }}> 질문하기 </Link>
         </div>
-				<div className="post-list-container">
-					{posts.map(post => (
-          	<PostLayout key={post.postId} post={post} />
-        	))}
-				</div>
+        <div className="post-list-container">
+          {posts.map(post => (
+            <PostLayout key={post.postId} post={post} />
+          ))}
+        </div>
       </div>
       <div className="pagination">
         {[...Array(totalPages).keys()].map(pageNumber => (
@@ -56,7 +63,7 @@ const BoardMain = () => {
         ))}
       </div>
     </div>
-  );
-};
+  )
+}
 
 export default BoardMain;
