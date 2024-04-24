0a701d78a940394ffce0a871e4e070823de91832
kbsksy1221 <kbsksy1221@naver.com>
2024-04-24 23:43:07 +0900

    fix: 게시글 상세보기 수정

--- src/pages/Code/PostDetailReq.jsx
+++ src/pages/Code/PostDetailReq.jsx
@@ -9,10 +9,6 @@ function PostDetailReq() {
   const [post, setPost] = useState(null);
 	const redirect_uri = import.meta.env.VITE_BACK_REDIRECT_URI;
 	
-	if (!post) {
-    return <div>Loading...</div>;
-  }
-	
   // const handleAnswer = () => {
   //   navigate(`/post/preview?codeId=${codeId}`);
   // };
@@ -37,31 +33,37 @@ function PostDetailReq() {
   }, [postId]);
 
   return (
-		<div className='postDetail-container'>
-			<h1>{post.title}</h1>
-			<div style={{display:"flex", justifyContent:"center", flexDirection:"row"}}>
-				<div className="post-information" style={{padding:"10px"}}>
-					<div style={{backgroundColor:"#FF9E9E", width:"500px", height:"500px"}}>
-						<img src={`data:image/png;base64,${post.imgBase64}`} alt="사진" />
-					</div>
-					<p>{post.name}</p>
-					<p>{post.content}</p>
-				</div>
-				<div className='answerList-container' style={{border:"1px solid black", margin:"10px", padding:"10px", width:"800px", height:"600px"}}>
-					<div className='answerDetail' style={{ backgroundColor:"#d5d5d5"}}>
-						<div style={{display:"flex", flexDirection:"row",  justifyContent:"space-between", alignItems:"center"}}>
-							<img alt='결과물 사진' style={{margin:"5px"}}/>
-							<div>
-								<p>답변자 닉네임</p>
-								<p>작성된 날짜</p>
-							</div>
-							<button style={{width:"60px", height:"40px", marginRight:"10px", fontSize:"20px"}}> 코드 미리보기 </button>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-  );
+  <div className='postDetail-container'>
+    {post ? (
+      <div>
+        <h1>{post.title}</h1>
+        <div style={{display:"flex", justifyContent:"center", flexDirection:"row"}}>
+          <div className="post-information" style={{padding:"10px"}}>
+            <div style={{backgroundColor:"#FF9E9E", width:"500px", height:"500px"}}>
+              <img src={`data:image/png;base64,${post.imgBase64}`} alt="사진" />
+            </div>
+            <p>{post.name}</p>
+            <p>{post.content}</p>
+          </div>
+          <div className='answerList-container' style={{border:"1px solid black", margin:"10px", padding:"10px", width:"800px", height:"600px"}}>
+            <div className='answerDetail' style={{ backgroundColor:"#d5d5d5"}}>
+              <div style={{display:"flex", flexDirection:"row",  justifyContent:"space-between", alignItems:"center"}}>
+                <img alt='결과물 사진' style={{margin:"5px"}}/>
+                <div>
+                  <p>답변자 닉네임</p>
+                  <p>작성된 날짜</p>
+                </div>
+                <button style={{width:"60px", height:"40px", marginRight:"10px", fontSize:"20px"}}> 코드 미리보기 </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    ) : (
+      <div>Loading...</div>
+    )}
+  </div>
+);
 }
 
 export default PostDetailReq;
