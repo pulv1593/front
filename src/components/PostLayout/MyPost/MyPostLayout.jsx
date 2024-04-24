f11c3cd7b174adc0c150f35cfe3a5bb9ba735712
kbsksy1221 <kbsksy1221@naver.com>
2024-04-24 17:18:51 +0900

    feat: MyPostLayout.jsx, css 파일 수정 및 dropdown 버튼 링크 추가

--- src/components/PostLayout/MyPost/MyPostLayout.jsx
+++ src/components/PostLayout/MyPost/MyPostLayout.jsx
@@ -1,40 +1,44 @@
-import { Link } from "react-router-dom";
+import './MyPostLayout.css';
 import { useState } from 'react';
-import "./MyPostLayout.css";
+import { Link } from 'react-router-dom';
 
-  const MyPostLayout = ({post}) => {
-		const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+const MyPostLayout = ({ posts }) => {
+  const [dropdownPostId, setDropdownPostId] = useState(null);
 		
-    if (!post || post.length === 0) {
-      return <div className="post-container">작성한 게시글이 없습니다.</div>;
-    }
+  if (!posts || posts.length === 0) {
+    return <div className="post-container">작성한 게시글이 없습니다.</div>;
+  }
 		
-		const toggleDropdown = () => {
-			setIsDropdownOpen(!isDropdownOpen);
-		};
-		
-    return (
-      <div className="post-container">
-        <Link to={`/post/${post.postId}`}>
+  const toggleDropdown = (postId) => {
+    setDropdownPostId(postId === dropdownPostId ? null : postId);
+  };
+
+  return (
+    <div className='postlist-container'>
+      {posts.map((post) => (
+        <div key={post.id} className="post-container">
           <div className="image-container">
             <img src={post.image} alt="이미지"/> 
           </div>
           <div className="post-info-container">
-						<div className="dropdown-container">
-							<button onClick={toggleDropdown} className="dropdown-toggle">
-								작성자: {post.member_id}
-							</button>
-							{isDropdownOpen && (
-								<div className="dropdown-menu">
-									<Link to={`/chat/start?member_id=${post.member_id}`} className="reciver-chat-link">쪽지 보내기</Link>
-								</div>
-							)}
-						</div>
-            <p>{post.title}</p>
+            <div className="dropdown-container">
+              <button onClick={() => toggleDropdown(post.postId)} className="dropdown-toggle">
+                작성자: {posts.member_id}
+              </button>
+              {dropdownPostId === post.postId && (
+                <div className="dropdown-menu">
+                  <Link to={`/chat/start?member_id=${posts.member_id}`} className="receiver-chat-link">쪽지 보내기</Link>
+                </div>
+              )}
+            </div>
+            <div className='post-title'>
+              <p>제목: {post.title}</p>
+            </div>
           </div>
-        </Link>
-      </div>
-    );
-  };
-  
+        </div>
+      ))}
+    </div>
+  );
+}
+
 export default MyPostLayout;
