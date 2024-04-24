2fcca754df220af95c098a567f6aa9cb865ed8a8
kbsksy1221 <kbsksy1221@naver.com>
2024-04-24 20:29:00 +0900

    fix: codeIDE.jsx 수정

--- src/pages/Code/CodeIDE.jsx
+++ src/pages/Code/CodeIDE.jsx
@@ -1,4 +1,4 @@
-import { useNavigate, useParams, useEffect } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import Editor from "../../components/CodeEditor/Editor";
 
 // component
