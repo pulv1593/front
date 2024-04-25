be65d1d6b12fcfb5950377dfd7ee25f242176fa3
pulv1593 <jiro0727@naver.com>
2024-04-25 15:09:17 +0900

    add: theme color 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 export default {
   content: ["./src/**/*.{js,jsx,ts,tsx}", "./index.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'custom-pink-1': '#FFE5EC',
+        'custom-pink-2': '#FFC2D1',
+        'custom-pink-3': '#FFB3C6',
+        'custom-pink-4': '#FF8FAB',
+        'custom-pink-5': '#FB6F92',
+      },
+    },
   },
   plugins: [],
 };
